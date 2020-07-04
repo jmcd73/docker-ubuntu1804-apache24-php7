0aeed98c801ddd1d256ecd0966fe082ce035eb40
James McDonald <james@toggen.com.au>
2020-07-04 23:55:26 +0000

    Docker prod build

--- docker-build.sh
+++ docker-build.sh
@@ -6,7 +6,10 @@
 if [ -f .docker-env ] ;
 then
     . ./.docker-env
+else
+    echo Please configure the .docker-env file 
+    exit 1
 fi
 
-docker build --network host -t ${DOCKER_TAG} .
+docker build --network host -f Dockerfile-${BUILD} -t ${DOCKER_TAG} .
 
